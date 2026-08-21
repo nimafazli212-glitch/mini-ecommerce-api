89db0801772564f3fd40e8ce767d9b18ce7f5e75
nimafazli212-glitch <nimafazli212@gmail.com>
2026-08-21 21:54:35 +0330

    Add API version

--- app/main.py
+++ app/main.py
@@ -26,5 +26,6 @@ app.include_router(orders_router)
 @app.get("/")
 async def root():
     return {
-        "message": "Mini E-Commerce API"
+        "message": "Mini E-Commerce API",
+        "version": "1.0.0"
     }
