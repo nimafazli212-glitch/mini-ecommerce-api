f330791c02eb3361db85f42fe85d0a5461749722
nimafazli212-glitch <nimafazli212@gmail.com>
2026-08-21 22:19:11 +0330

    Change version to 2.0.0

--- app/main.py
+++ app/main.py
@@ -27,5 +27,5 @@ app.include_router(orders_router)
 async def root():
     return {
         "message": "Mini E-Commerce API - Discount Feature",
-        "version": "1.0.0"
+        "version": "2.0.0"
     }
